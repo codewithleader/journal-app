55bcef33d902e042ad91ba83802180db973dd7ee
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-05-05 15:37:33 -0500

    Form error handling

--- README.md
+++ README.md
@@ -24,4 +24,10 @@
  - The registration options by email and Google were enabled.
  - Installed Firebase v8.10.0 using npm command.
 - Configure Firebase and Google Sign-in.
+
+### Forms
 - User registration form.
+- Form error handling.
+ - Implementing the "validator" library to validate the form fields.
+  - validator.isEmail returns boolean.
+   - the !validator.isEmail is used with the exclamation mark.
--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,31 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useForm } from '../../hooks/useForm';
+import validator from 'validator'
 
 export const RegisterScreen = () => {
 
 	const [formValues, handleInputChange] = useForm({
 		name: 'Elis Antonio',
 		email: 'elis@mail.com',
-		password: '1234',
-		password2: '1234',
+		password: '123456',
+		password2: '123456',
 	});
 
 	const { name, email, password, password2 } = formValues;
 
 	const handleRegister = (e) => {
 		e.preventDefault();
-
+		if (isFormValid()) {
+			console.log('formulario correcto');
+		}
 	}
 
 	const isFormValid = () => {
-		
-	}
+		if (name.trim().length === 0) {
+			console.log('name is required');
+			return false;
+		} else if (!validator.isEmail(email)) {
+			console.log('email is not valid');
+			return false;
+		} else if (password !== password2 || password.length < 5) {
+			console.log('password should be at least 6 characters and match each other');
+			return false;
+		}
+
+		return true;
+	}// cierre isFormValid
 
 	return (
 		<>
 			<h3 className='auth__title'>Register</h3>
-			<form onSubmit={handleRegister}>
+			<form onSubmit={ handleRegister }>
+				
+				<div className="auth__alert-error">
+					hola mundo
+				</div>
+
 				<input
 					autoComplete='off'
 					className='auth__input'
