f20dcd256e7192e830a6d710e0c597244592c628
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-05-19 10:32:18 -0500

    test: 🧪 end test
    add MIT LICENSE and finish testing

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -5,79 +5,78 @@ import { startGoogleLogin, startLoginEmailPassword } from '../../actions/auth';
 import { useForm } from '../../hooks/useForm';
 
 export const LoginScreen = () => {
+  const dispatch = useDispatch();
+  const { loading } = useSelector(state => state.ui);
 
-	const dispatch = useDispatch();
-	const { loading } = useSelector( state => state.ui );
+  const [formValues, handleInputChange] = useForm({
+    email: '',
+    password: '',
+  });
 
-	const [formValues, handleInputChange] = useForm({
-		email: '',
-		password: ''
-	});
+  const { email, password } = formValues;
 
-	const { email, password } = formValues;
+  const handleLogin = e => {
+    e.preventDefault();
+    dispatch(startLoginEmailPassword(email, password));
+  };
+  // shortcut for functions such as the below: nf ó nfn
+  const handleGoogleLogin = () => {
+    dispatch(startGoogleLogin());
+  };
 
-	const handleLogin = (e) => {
-		e.preventDefault();
-		dispatch(startLoginEmailPassword(email, password));
+  return (
+    <>
+      <h3 className='auth__title'>Login</h3>
+      <form
+        className='animate__animated animate__fadeIn animate__faster'
+        onSubmit={handleLogin}
+      >
+        <input
+          autoComplete='off'
+          className='auth__input'
+          name='email'
+          placeholder='Email'
+          type='text'
+          value={email}
+          onChange={handleInputChange}
+        />
+        <input
+          autoComplete='off'
+          className='auth__input'
+          name='password'
+          placeholder='Password'
+          type='password'
+          value={password}
+          onChange={handleInputChange}
+        />
+        <button
+          className='btn btn-primary btn-block'
+          disabled={loading}
+          type='submit'
+        >
+          Login
+        </button>
 
-	}
-	// shortcut for functions such as the below: nf ó nfn
-	const handleGoogleLogin = () => {
-		dispatch(startGoogleLogin());
-	}
-	
+        <div className='auth__social-networks'>
+          <p>Login with social networks</p>
 
-	return (
-		<>
-			<h3 className='auth__title'>Login</h3>
-			<form className='animate__animated animate__fadeIn animate__faster' onSubmit={handleLogin}>
-				<input
-					autoComplete='off'
-					className='auth__input'
-					name='email'
-					placeholder='Email'
-					type='text'
-					value={email}
-					onChange={handleInputChange}
-				/>
-				<input
-					autoComplete='off'
-					className='auth__input'
-					name='password'
-					placeholder='Password'
-					type='password'
-					value={password}
-					onChange={handleInputChange}
-				/>
-				<button
-					className='btn btn-primary btn-block'
-					disabled={loading}
-					type='submit'
-				>
-					Login
-				</button>
-				
-				<div className='auth__social-networks'>
-					<p>Login with social networks</p>
-					
-					<div
-						className='google-btn'
-						onClick={ handleGoogleLogin }
-					>
-						<div className='google-icon-wrapper'>
-							<img
-								className='google-icon'
-								src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
-								alt='google button'
-							/>
-						</div>
-						<p className='btn-text'>
-							<b>Sign in with google</b>
-						</p>
-					</div>
-				</div>
-				<Link className='link' to='/auth/register'>Create new account</Link>
-			</form>
-		</>
-	);
+          <div className='google-btn' onClick={handleGoogleLogin}>
+            <div className='google-icon-wrapper'>
+              <img
+                className='google-icon'
+                src='https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg'
+                alt='google button'
+              />
+            </div>
+            <p className='btn-text'>
+              <b>Sign in with google</b>
+            </p>
+          </div>
+        </div>
+        <Link className='link' to='/auth/register'>
+          Create new account
+        </Link>
+      </form>
+    </>
+  );
 };
